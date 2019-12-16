5ee1f2718cf1585de4e076470b2597e5357cb5e6
habibahassan <halimaadan92@gmail.com>
2019-12-16 18:02:49 +0300

    add indentation

--- js/scripts.js
+++ js/scripts.js
@@ -3,10 +3,10 @@ $("document").ready(function () {
         var sizeAmount = parseInt($("#size").val());
         var crustAmount = parseInt($("#crust").val());
         var toppingsAmount = parseInt($("#toppings").val());
-        var sumTotal = sizeAmount+crustAmount+toppingsAmount;
+        var sumTotal = sizeAmount + crustAmount + toppingsAmount;
         var quantity = parseInt($("#number").val());
         var totalPrice = sumTotal * quantity;
-        alert("This is your total cost "+totalPrice);
+        alert("This is your total cost thank you for ordering " + totalPrice);
 
     });
     $("#delivery").submit(function (event) {
@@ -16,11 +16,11 @@ $("document").ready(function () {
         var location = $("input#location").val();
         console.log(name)
 
-        alert("Hello " + name + ". Your order has been successfuly received and will be delivered to " +  location + " within one hour.The delivery will cost ksh 180/= Thank you for choosing Habib pizza .");
+        alert("Hello " + name + ". Your order has been successfuly received and will be delivered to " + location + " within one hour.The delivery will cost ksh 180/= Thank you for choosing Habib pizza .");
         // $(this).get(0).reset();
-         event.preventDefault();
+        event.preventDefault();
     });
- 
+
 
 
 
